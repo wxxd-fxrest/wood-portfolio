e0b8667a572c2f92371b4936a24531f5fb4b3cdf
fxrest704 <fxrest704@gmail.com>
2023-08-28 18:48:51 +0900

    레이아웃

--- src/App.js
+++ src/App.js
@@ -1,28 +1,92 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import Intro from './components/Intro';
 import Project from './components/Project';
 import Start from './components/Start';
 
 const App = () => {
-  return (
-		<Container>
-            <Component>
-                <Start />
-            </Component>
-            <Component>
-                <Intro />
-            </Component>
-            <Component>
-                <Project />
-            </Component>
-		</Container>
-	);
+    const [currentPage, setCurrentPage] = useState(0);
+    const sections = [
+        { id: 'Start', color: '#dc322f' },
+        { id: 'Intro', color: '#268bd2' },
+        { id: 'Project', color: '#073642' },
+    ];
+    const containerRef = useRef();
+
+    const scrollToSection = (index) => {
+        const targetSection = document.getElementById(sections[index].id);
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+            });
+        }
+    };
+
+    const handleScroll = useCallback((e) => {
+        const delta = e.deltaY;
+        const newPageIndex = currentPage + (delta > 0 ? 1 : -1);
+    
+        if (newPageIndex >= 0 && newPageIndex < sections.length) {
+            setCurrentPage(newPageIndex);
+            scrollToSection(newPageIndex);
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentPage]);
+    
+    useEffect(() => {
+        const container = containerRef.current;
+        const scrollHandler = (e) => handleScroll(e);
+        container.addEventListener('wheel', scrollHandler);
+        return () => {
+            container.removeEventListener('wheel', scrollHandler);
+        };
+    }, [handleScroll]);
+
+    return (
+        <Container ref={containerRef}>
+            {sections.map((section, index) => (
+                <Section
+                    key={section.id}
+                    id={section.id}
+                    className={`section ${currentPage === index ? 'active' : ''}`}
+                    color={section.color}
+                >
+                    {section.id === 'Start' && <Start />}
+                    {section.id === 'Intro' && <Intro />}
+                    {section.id === 'Project' && <Project />}
+                </Section>
+            ))}
+        </Container>
+    );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    overflow: hidden;
+    scroll-snap-type: y mandatory;
+    overflow-y: scroll;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+    cursor: pointer;
+`;
 
-const Component = styled.div``;
+const Section = styled.div`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    scroll-snap-align: start;
+    transition: background-color 0.3s;
 
+    background-color: ${({ color }) => color};
+    
+    h1 {
+        color: white;
+        font-size: 3rem;
+    }
+`;
 
 export default App;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,14 +1,201 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Project = () => {
-    return(
+    const projectData = [
+        { bgColor: 'skyblue', name: 'Project-1' },
+        { bgColor: 'pink', name: 'Project-2' },
+        { bgColor: 'lightgreen', name: 'Project-3' },
+        // ... 추가 프로젝트 데이터
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleNextClick = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % projectData.length);
+    };
+
+    const handlePrevClick = () => {
+        setCurrentIndex((prevIndex) =>
+            prevIndex === 0 ? projectData.length - 1 : prevIndex - 1
+        );
+    };
+
+    return (
         <Container>
-            <p> Project </p>
+            <ProjectBox style={{ backgroundColor: projectData[currentIndex].bgColor }}>
+                <ProjectImg />
+                <ProjectName>{projectData[currentIndex].name}</ProjectName>
+            </ProjectBox>
+            <ControlButtons>
+                <BeforeBtn onClick={handlePrevClick}>이전</BeforeBtn>
+                <AfterBtn onClick={handleNextClick}>다음</AfterBtn>
+            </ControlButtons>
+            <Dots>
+                {projectData.map((_, index) => (
+                    <Dot key={index} active={index === currentIndex} onClick={() => setCurrentIndex(index)} />
+                ))}
+            </Dots>
         </Container>
-    )
+    );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const ProjectBox = styled.div`
+    width: 300px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ProjectImg = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: #ccc;
+    margin-bottom: 10px;
+`;
+
+const ProjectName = styled.h1`
+    font-size: 1rem;
+    text-align: center;
+`;
+
+const ControlButtons = styled.div`
+    display: flex;
+    margin-top: 10px;
+`;
+
+const BeforeBtn = styled.button`
+    margin-right: 10px;
+`;
+
+const AfterBtn = styled.button``;
+
+const Dots = styled.div`
+    display: flex;
+    margin-top: 10px;
+`;
+
+const Dot = styled.div`
+    width: 10px;
+    height: 10px;
+    background-color: ${({ active }) => (active ? "#000" : "#ccc")};
+    border-radius: 50%;
+    margin: 0 5px;
+    cursor: pointer;
+`;
 
 export default Project;
+
+
+// import React, { useState } from 'react';
+// import styled from 'styled-components';
+
+// const Project = () => {
+//     const projectData = [
+//         { bgColor: 'skyblue', name: 'Project-1' },
+//         { bgColor: 'pink', name: 'Project-2' },
+//         { bgColor: 'lightgreen', name: 'Project-3' },
+//         { bgColor: 'lightblue', name: 'Project-4' },
+//         { bgColor: 'orange', name: 'Project-5' },
+//         { bgColor: 'purple', name: 'Project-6' },
+
+//         { bgColor: 'skyblue', name: 'Project-7' },
+//         { bgColor: 'pink', name: 'Project-8' },
+//         { bgColor: 'lightgreen', name: 'Project-9' },
+//         { bgColor: 'lightblue', name: 'Project-10' },
+//         { bgColor: 'orange', name: 'Project-11' },
+//         { bgColor: 'purple', name: 'Project-12' },
+//     ];
+
+//     const itemsPerPage = 3; // 한 페이지에 보여질 아이템 개수
+//     const [currentPage, setCurrentPage] = useState(0);
+
+//     const handleNextClick = () => {
+//         setCurrentPage((prevPage) => prevPage + 1);
+//     };
+
+//     const handlePrevClick = () => {
+//         setCurrentPage((prevPage) => prevPage - 1);
+//     };
+
+//     const startIndex = currentPage * itemsPerPage;
+//     const visibleItems = projectData.slice(startIndex, startIndex + itemsPerPage);
+
+//     return (
+//         <Container>
+//             <ProjectBoxWrapper>
+//                 {visibleItems.map((item, index) => (
+//                     <ProjectBox key={index} style={{ backgroundColor: item.bgColor }}>
+//                         <ProjectImg />
+//                         <ProjectName>{item.name}</ProjectName>
+//                     </ProjectBox>
+//                 ))}
+//             </ProjectBoxWrapper>
+//             <ButtonWrapper>
+//                 <BeforeBtn onClick={handlePrevClick} disabled={currentPage === 0}>
+//                     이전
+//                 </BeforeBtn>
+//                 <AfterBtn
+//                     onClick={handleNextClick}
+//                     disabled={startIndex + itemsPerPage >= projectData.length}
+//                 >
+//                     다음
+//                 </AfterBtn>
+//             </ButtonWrapper>
+//         </Container>
+//     );
+// };
+
+// const Container = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+// `;
+
+// const ProjectBoxWrapper = styled.div`
+//     display: flex;
+//     overflow-x: auto;
+// `;
+
+// const ProjectBox = styled.div`
+//     width: 300px;
+//     height: 200px;
+//     margin: 0 10px;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     justify-content: center;
+// `;
+
+// const ProjectImg = styled.div`
+//     width: 100px;
+//     height: 100px;
+//     background-color: #ccc;
+//     margin-bottom: 10px;
+// `;
+
+// const ProjectName = styled.h1`
+//     font-size: 1rem;
+//     text-align: center;
+// `;
+
+// const ButtonWrapper = styled.div`
+//     display: flex;
+//     margin-top: 10px;
+// `;
+
+// const BeforeBtn = styled.button`
+//     margin-right: 10px;
+// `;
+
+// const AfterBtn = styled.button``;
+
+// export default Project;
